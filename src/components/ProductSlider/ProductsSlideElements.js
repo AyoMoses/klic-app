027f6085c456066186716ea2bfbe7f862ebf1b75
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2021-03-08 10:19:05 +0100

    design sktech for product slider web and mobile

--- src/components/ProductSlider/ProductsSlideElements.js
+++ src/components/ProductSlider/ProductsSlideElements.js
@@ -1,34 +1,103 @@
 import styled from 'styled-components'
+import bg from '../../assets/images/radial-bg.png'
 
 export const ProductSlideContainer = styled.div`
     width: 100%;
     max-width: calc(100% - 120px);
     margin: 0 auto;
+    margin-top: 120px;
     display: grid;
     grid-template-columns: 1fr 2fr;
-    height: 1300px;
-    background: darkorange;
+
+    @media screen and (max-width: 768px) {
+        display: flex;
+        flex-direction: column-reverse;
+        gap: 50px 0;
+        max-width: calc(100% - 48px);
+        margin-top: 0;
+        height: auto;
+    }
 `
 
 export const SlideThumbnailsWrap = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 100px 0;
+    gap: 50px 0;
 `
 
 export const ThumbnailWrap = styled.div`
     width: 100%;
-    height: 100px;
-    &:nth-child(even) {
-        background-color: darkseagreen;
-    }
-    &:nth-child(odd) {
-        background-color: darkred;
-    }
+    display: flex;
+    flex-direction: row;
+    gap: 0 15px;
+`
+
+export const SlideIcon = styled.img`
+    width: 50px;
+    height: 50px;
+`
+export const SlideContentWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const SlideHeaderInactive = styled.div`
+    color: #5a595b;
+    font-size: 1.375rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 32px;
+    letter-spacing: 0px;
+    text-align: left;
+`
+
+export const SlideHeaderActive = styled.div`
+    color: #ffffff;
+    font-size: 1.625rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 32px;
+    letter-spacing: 0px;
+    text-align: left;
+`
+
+export const SlideTextActive = styled.div`
+    color: #ffffff;
+    font-size: 1.125rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 26px;
+    letter-spacing: 0px;
+    text-align: left;
+    opacity: 0.8;
+    margin-top: 19px;
+`
+
+export const Block = styled.span`
+    display: block;
 `
 
 export const ProductMainImageWrap = styled.div`
     width: 100%;
     height: 100%;
-    background: red;
+    background: url(${bg});
+    background-position: center;
+    background-size: cover;
+    display: grid;
+    place-items: center;
+
+    @media screen and (max-width: 768px) {
+        height: auto;
+    }
+`
+
+export const SlideImage = styled.img`
+    width: 700px;
+    max-width: 750px;
+    height: auto;
+
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        max-width: 100%;
+    }
 `
--- src/components/ProductSlider/productslide.js
+++ src/components/ProductSlider/productslide.js
@@ -1,20 +1,75 @@
 import React from 'react'
-import { ProductSlideContainer, SlideThumbnailsWrap, ThumbnailWrap, ProductMainImageWrap } from './ProductsSlideElements'
+import { ProductSlideContainer, SlideThumbnailsWrap, ThumbnailWrap, ProductMainImageWrap, SlideImage, SlideIcon, SlideContentWrap, SlideHeaderInactive, SlideHeaderActive, SlideTextActive, Block } from './ProductsSlideElements'
+
+import iconOne from '../../assets/icons/slide-icon-one.svg'
+import iconTwo from '../../assets/icons/slide-icon-two.svg'
+import iconThree from '../../assets/icons/slide-icon-three.svg'
+import iconFour from '../../assets/icons/slide-icon-four.svg'
+import iconFive from '../../assets/icons/slide-icon-five.svg'
+import iphone from '../../assets/images/iPhone_Landscape.png'
 
 const ProductSlide = () => {
     return (
         <>
             <ProductSlideContainer>
                 <SlideThumbnailsWrap>
-                    <ThumbnailWrap></ThumbnailWrap>
-                    <ThumbnailWrap></ThumbnailWrap>
-                    <ThumbnailWrap></ThumbnailWrap>
-                    <ThumbnailWrap></ThumbnailWrap>
-                    <ThumbnailWrap></ThumbnailWrap>
-                    <ThumbnailWrap></ThumbnailWrap>
-                    <ThumbnailWrap></ThumbnailWrap>
+
+                    <ThumbnailWrap>
+                        <SlideIcon src={iconOne} alt="slide icon"></SlideIcon>
+                        <SlideContentWrap>
+                            <SlideHeaderInactive>All Eyes On Your Brand</SlideHeaderInactive>
+                        </SlideContentWrap>
+                    </ThumbnailWrap>
+
+                    <ThumbnailWrap>
+                        <SlideIcon src={iconTwo} alt="slide icon"></SlideIcon>
+                        <SlideContentWrap>
+                            <SlideHeaderInactive>Highly Customizable White <Block>Label Software</Block></SlideHeaderInactive>
+                        </SlideContentWrap>
+                    </ThumbnailWrap>
+
+                    <ThumbnailWrap>
+                        <SlideIcon src={iconTwo} alt="slide icon"></SlideIcon>
+                        <SlideContentWrap>
+                            <SlideHeaderInactive>Your Brand Front and Center</SlideHeaderInactive>
+                        </SlideContentWrap>
+                    </ThumbnailWrap>
+
+                    <ThumbnailWrap>
+                        <SlideIcon src={iconThree} alt="slide icon"></SlideIcon>
+                        <SlideContentWrap>
+                            <SlideHeaderActive>View Multiple Streams at Once</SlideHeaderActive>
+                            <SlideTextActive>
+                                Can’t decide which stream to watch? No problem! Klic allows you to watch multiple streams ath the same time.
+                            </SlideTextActive>
+                        </SlideContentWrap>
+                    </ThumbnailWrap>
+
+                    <ThumbnailWrap>
+                        <SlideIcon src={iconTwo} alt="slide icon"></SlideIcon>
+                        <SlideContentWrap>
+                            <SlideHeaderInactive>The Fastest Multi <Block>Stream Experience Possible</Block></SlideHeaderInactive>
+                        </SlideContentWrap>
+                    </ThumbnailWrap>
+
+                    <ThumbnailWrap>
+                        <SlideIcon src={iconFour} alt="slide icon"></SlideIcon>
+                        <SlideContentWrap>
+                            <SlideHeaderInactive>Pick Your Path Viewing <Block>Experience</Block></SlideHeaderInactive>
+                        </SlideContentWrap>
+                    </ThumbnailWrap>
+
+                    <ThumbnailWrap>
+                        <SlideIcon src={iconFive} alt="slide icon"></SlideIcon>
+                        <SlideContentWrap>
+                            <SlideHeaderInactive>Pick Your Path Viewing <Block>Experience</Block></SlideHeaderInactive>
+                        </SlideContentWrap>
+                    </ThumbnailWrap>
+
                 </SlideThumbnailsWrap>
-                <ProductMainImageWrap></ProductMainImageWrap>
+                <ProductMainImageWrap>
+                    <SlideImage src={iphone} alt="about our service"></SlideImage>
+                </ProductMainImageWrap>
             </ProductSlideContainer>
         </>
     )
